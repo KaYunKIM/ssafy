800f96a628ddb3f6126d53de1c801025ade514d8
KaYunKIM <gayun1109@hanmail.net>
2020-03-17 23:15:04 +0900

    2020.03.17

---
+++ kayunkim/homeworkshop/HTML&CSS 레이아웃/03.16/index.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('form');
+const users = ['admin', 'ssafy', 'test'];
+
+form.addEventListener('submit', function(event) {
+  // 각 변수에 담기 위한 코드 입니다. 수정을 금합니다.
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const userName = formData.get('username');
+  const password = formData.get('password');
+  const passwordConfirmation = formData.get('password_confirmation');
+  // 개발자 도구를 통해 해당 값들을 모두 확인하세요.
+  console.log(userName, password, passwordConfirmation);
+  // 위의 값들을 활용하여, 회원가입 로직을 작성하시오.
+  if (users.includes(userName)){
+    alert("존재하는 회원입니다.");
+  }
+  else{
+    if (password !== passwordConfirmation){
+      alert("비밀번호가 일치하지 않습니다.");
+      form.password.value = "";
+      form.passwordConfirmation.value= "";
+    }
+    else{
+      alert(userName+"님, 회원가입을 축하합니다.");
+    }
+  }
+  })
+
+
+  // function check(userName,password,passwordConfirmation) {
+  //   for( const i = 0; i <users.length; i++) {
+  //     if (users[i] === userName) {
+  //       form.userName.value="";
+  //       return alert("존재하는 회원입니다.");
+  //     }
+  //   }
+  //   if  {
+  //     alert("비밀번호가 일치하지 않습니다.");
+  //     form.password.value = "";
+  //     form.passwordConfirmation.value = "";
+  //     form.passwordConfirmation.focus();
+  //     return false;
+  //   return true;
+  //   }
+  // }
+  // if function(userName,password,passwordConfirmation){
+  //   alert("회원가입을 축하합니다.");
+  // }
