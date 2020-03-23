a97d7e13dff514dcb15820e1b75cf0a18eb96922
KaYunKIM <gayun1109@hanmail.net>
2020-03-24 00:15:37 +0900

    2020.03.24

--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Intermediate/STACK2/4874. Forth.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Intermediate/STACK2/4874. Forth.py
@@ -1,1 +1,29 @@
+def forth():
+    stack = []
+    for i in lst:
+        if i == '+' or i == '-' or i == '*' or i == '/':
+            if len(stack)>= 2:
+                b = int(stack.pop())
+                a = int(stack.pop())
+                if i == '+':
+                    stack.append(a+b)
+                elif i == '-':
+                    stack.append(a-b)
+                elif i == '*':
+                    stack.append(a*b)
+                elif i == '/':
+                    stack.append(a//b)
+            else:
+                return 'error'
+        elif i == '.':
+            if len(stack)== 1:
+                return stack.pop()
+            else:
+                return 'error'
+        else:
+             stack.append(i)
+
 T = int(input())
+for tc in range(1,T+1):
+    lst= list(input().split())
+    print('#{} {}'.format(tc,forth()))
