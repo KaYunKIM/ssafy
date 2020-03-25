6b1c4e3ebd3a451f5ca82b899d91fbb05d412d05
KaYunKIM <gayun1109@hanmail.net>
2020-03-25 18:06:12 +0900

    2020.03.25

---
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Intermediate/STACK2/4875. 미로.py
@@ -0,0 +1,32 @@
+def maze(row,col,N):
+    stack = [[row,col]]
+    mtx[row][col] = 1
+
+    di = [1, 0, -1]
+    dj = [-1, 1, 0]
+
+    while stack:
+        nrow,ncol = stack.pop()
+        if mtx[nrow][ncol] == 3:
+            return 1
+        for d in range(3):
+            newi = nrow + di[d]
+            newj = ncol + dj[d]
+
+            if 0 <= newi < N and 0 <= newj < N:
+                if mtx[newi][newj] != 1:
+                    stack.append([newi,newj])
+                    mtx[nrow][ncol] = 1
+    return 0
+
+T = int(input())
+for tc in range(1,T+1):
+    N = int(input())
+    mtx = [list(input()) for _ in range(N)]
+    for i in range(N-1,-1,-1):
+        for j in range(N):
+            print(mtx[i][j])
+            if mtx[i][j] == 2:
+                print(i,j)
+    #             row,col = i,j
+    # print(maze(row,col,N))
