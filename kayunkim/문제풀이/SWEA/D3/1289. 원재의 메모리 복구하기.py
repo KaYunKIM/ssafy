b40572d4b2eb9407c88f0b64f26d3fb05aa352f4
KaYunKIM <gayun1109@hanmail.net>
2020-04-05 17:58:44 +0900

    2020.04.05

--- kayunkim/문제풀이/SWEA/D3/1289. 원재의 메모리 복구하기.py
+++ kayunkim/문제풀이/SWEA/D3/1289. 원재의 메모리 복구하기.py
@@ -1,15 +1,24 @@
+# T = int(input())
+# for tc in range(1,T+1):
+#     org = list(map(int,input()))
+#     cnt = 0
+#     ans = []
+#     if org[0] == 1:
+#         cnt+=1
+#     for i in range(len(org)-1):
+#         if org[i] != org[i+1]:
+#             cnt+=1
+#     print('#{} {}'.format(tc,cnt))
+
 T = int(input())
 for tc in range(1,T+1):
     org = list(map(int,input()))
+    cur = org[0]
     cnt = 0
-    for i in range(len(org)):
-        if int(org[i]) == 1:
-            bin[i::]=1
-        elif int(org[i]) == 0:
-            bin[i::] =0
-        print(bin)
-        cnt += 1
-        print(cnt)
-    print(cnt)
-
-
+    if org[0] == 1:
+        cnt+=1
+    for i in range(1,len(org)):
+        if cur!= org[i]:
+            cnt+=1
+            cur = org[i]
+    print('#{} {}'.format(tc,cnt))
