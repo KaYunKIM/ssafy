de2cc754e3b1fe19481992f00a2255b4bb59f26c
KaYunKIM <gayun1109@hanmail.net>
2020-04-08 22:18:37 +0900

    2020.04.08

--- kayunkim/문제풀이/SWEA/D3/1216. 회문2.py
+++ kayunkim/문제풀이/SWEA/D3/1216. 회문2.py
@@ -1,8 +1,14 @@
-for tc in range(1,T+1):
-    mtx = [list(map(input())) for _ in range(100)]
-    mtx += [[mxt[x][y] for x in range(100)] for y in range(100)]
+for tc in range(1,11):
+    N = int(input())
+    mtx = [list(input()) for _ in range(100)]
+    mtx += [[mtx[x][y] for x in range(100)] for y in range(100)]
     max = 0
     for i in range(200):
         for j in range(100):
-            if mtx[] == mtx[::-1]:
-                
+            for k in range(1,100-j+1):
+                if mtx[i][j:j+k] == mtx[i][j+k-1:j-1:-1]:
+                    if max< len(mtx[i][j:j+k]):
+                        max = len(mtx[i][j:j+k])
+
+    print(max)
+
