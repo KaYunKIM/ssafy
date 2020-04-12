02db5070c1e2c945a67a6f89ac6c636e54116320
KaYunKIM <gayun1109@hanmail.net>
2020-04-12 13:51:27 +0900

    2020.04.12

--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Intermediate/LINKED LIST/sample.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Intermediate/LINKED LIST/sample.py
@@ -1,15 +1,15 @@
 arr = [69, 10, 30, 2, 16, 8, 31, 22]
 N = len(arr)
 
-# for i in range(1, N):        #1~N -1
-#     temp = arr[i]
-#     j = i-1
-#     while j >= 0 and arr[j] >temp:
-#         arr[j+1] = arr[j]
-#         j-=1
-#     arr[j+1] = temp
-#
-#     print(arr)
+for i in range(1, N):        #1~N -1
+    temp = arr[i]
+    j = i-1
+    while j >= 0 and arr[j] >temp:
+        arr[j+1] = arr[j]
+        j-=1
+    arr[j+1] = temp
+
+    print(arr)
 
 for i in range(1,N):
     temp = arr[i]
