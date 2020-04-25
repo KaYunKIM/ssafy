393a69aad22b749e30a26f754cdb456bb658db78
KaYunKIM <gayun1109@hanmail.net>
2020-04-25 21:54:50 +0900

    2020.04.25

--- kayunkim/문제풀이/SWEA/D3/1213. String.py
+++ kayunkim/문제풀이/SWEA/D3/1213. String.py
@@ -4,3 +4,13 @@ for tc in range(1,11):
     sentence = input()
     ans = sentence.count(word)
     print('#{} {}'.format(tc,ans))
+
+# for tc in range(1,11):
+#     T = int(input())
+#     word = input()
+#     sentence = input()
+#     cnt = 0
+#     for i in range(len(sentence)-len(word)+1):
+#         if sentence[i:i+len(word)] == word:
+#             cnt+=1
+#     print('#{} {}'.format(tc,cnt))
