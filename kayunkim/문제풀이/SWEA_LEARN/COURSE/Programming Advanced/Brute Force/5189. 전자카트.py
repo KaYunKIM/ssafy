e19b85c68d6ec212b16be84153c8fd31451111db
KaYunKIM <gayun1109@hanmail.net>
2020-05-07 01:02:27 +0900

    2020.05.07

--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5189. 전자카트.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5189. 전자카트.py
@@ -1,36 +1,31 @@
 def find(num):
-    cnt = 0
     min = 100000000
     stack = [num]
     bin = ''
     while stack:
-        print(stack)
+        cnt = 0
+        # print(stack)
         cart = stack.pop()
         bin = cart
-        print(''.format(tc,bin))
-        if len(bin) == N-1:
+        if len(bin) == N:
             bin+='1'
-            for j in range(N-1):
-                print(j,j+1)
-                print(bin[j], bin[j+1])
+            for j in range(N):
+                # print(bin[j], bin[j+1])
                 cnt+= e[int(bin[j])-1][int(bin[j+1])-1]
+                # print('cnt:{}'.format(cnt))
             if cnt < min:
                 min = cnt
+            # print('min:{}'.format(min))
         else:
-            # if cnt+e[bin[k]][bin[k+1]] > min:
-            #     pass
-            # else:
             for i in range(2, N+1):
-                print(stack, i)
-                print('bin:{}'.format(tc,bin))
+                # print('bin:{}'.format(bin))
                 if str(i) not in bin:
-                    print(bin)
                     stack.append(bin+str(i))
-    print(min)
+    return min
 
 
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     e= [list(map(int,input().split())) for _ in range(N)]
-    find('1')
+    print('#{} {}'.format(tc,find('1')))
