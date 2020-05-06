973d9ed7b36a5787b7f6679e503bc0d0d463a0c0
KaYunKIM <gayun1109@hanmail.net>
2020-05-07 01:36:20 +0900

    2020.05.07

--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/123.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/123.py
@@ -1,5 +1,31 @@
-bin = ''
-bin+='1'
-ans = 1
-print(type(str(ans)))
-print(bin)
+def DFS(start):
+    global sub_result, result, final_result
+
+    if len(sub_result) == N-1:
+        for i, j in sub_result:
+            result += Battery[i][j]
+
+        result += Battery[start][0]
+        final_result.append(result)
+        result = 0
+        return
+
+    for next in range(1, N):
+        if not visited[next]:
+            sub_result.append((start, next))
+            visited[next] = True
+            DFS(next)
+            sub_result.remove((start, next))
+            visited[next] = False
+
+TC = int(input())
+for tc in range(1, TC+1):
+    N = int(input())
+    Battery = [list(map(int, input().split())) for _ in range(N)]
+    visited = [0] * N
+    sub_result =  []
+    result = 0
+    final_result =[]
+    DFS(0)
+
+    print('#{} {}'.format(tc, min(final_result)))
--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5189. 전자카트.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5189. 전자카트.py
@@ -4,21 +4,27 @@ def find(num):
     bin = ''
     while stack:
         cnt = 0
-        # print(stack)
+        print(stack)
         cart = stack.pop()
         bin = cart
         if len(bin) == N:
             bin+='1'
             for j in range(N):
-                # print(bin[j], bin[j+1])
-                cnt+= e[int(bin[j])-1][int(bin[j+1])-1]
-                # print('cnt:{}'.format(cnt))
+                print(bin[j], bin[j+1])
+                if cnt+e[int(bin[j])-1][int(bin[j+1])-1] < min:
+                    cnt+= e[int(bin[j])-1][int(bin[j+1])-1]
+                else:
+                    cnt = min
+                    break
+                print('cnt:{}'.format(cnt))
             if cnt < min:
                 min = cnt
-            # print('min:{}'.format(min))
+            else:
+                cnt = min
+            print('min:{}'.format(min))
         else:
             for i in range(2, N+1):
-                # print('bin:{}'.format(bin))
+                print('bin:{}'.format(bin))
                 if str(i) not in bin:
                     stack.append(bin+str(i))
     return min
