9f119c18e230d9bc87b9fe5b490d4ffe8fde5835
KaYunKIM <gayun1109@hanmail.net>
2020-05-16 04:35:00 +0900

    2020.05.16

--- kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5190. 전자카트.py
+++ kayunkim/문제풀이/SWEA_LEARN/COURSE/Programming Advanced/Brute Force/5190. 전자카트.py
@@ -1,25 +1,25 @@
-def find(num,cur,sumV):
+def find(n,cur,sumV):
     global minV
-    if num==N:
+    if n==N:
         sumV+=e[cur][0]
-        if minV > sumV:
+        if sumV< minV:
             minV = sumV
-            return
-    elif minV <= sumV:
         return
     else:
-        for i in range(1,N):
-            if v[i]==0:
-                v[i]=1
-                find(num+1,i,sumV+e[cur][i])
-                v[i]=0
-        return
+        if sumV> minV:
+            return
+        else:
+            for i in range(1,N):
+                if v[i]==0:
+                    v[i]=1
+                    find(n+1,i,sumV+e[cur][i])
+                    v[i]=0
 
 T = int(input())
 for tc in range(1,T+1):
     N = int(input())
     e = [list(map(int,input().split())) for _ in range(N)]
     minV = 10000000
-    v= [0]*(N+1)
+    v=[0]*(N+1)
     find(1,0,0)
     print('#{} {}'.format(tc,minV))
