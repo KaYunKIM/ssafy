813ea574f64f06e8647395598432cf93729274d4
KaYunKIM <gayun1109@hanmail.net>
2020-05-24 00:08:03 +0900

    2020.05.24

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.1/실패율.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.1/실패율.py
@@ -1,27 +1,10 @@
 def solution(N, stages):
-    answer = []
     bin = {}
-    for i in stages:
-        if i not in bin:
-            bin[i] = 1
-        else:
-            bin[i]+=1
-    new_stage = sorted(bin.items())
     user = len(stages)
-    bin2 = {}
-    for j in new_stage:
-        if j[0] == N+1:
-            bin2[j[0]-1]=0/user
+    for i in range(1,N+1):
+        if stages.count(i) !=0:
+            bin[i] = stages.count(i)/user
         else:
-            bin2[j[0]] = j[1]/user
-        user-=j[1]
-    # print(sorted(bin2.items()))
-    ans = sorted(bin2.items(), key=lambda x:x[1], reverse=True)
-    for i in ans:
-        answer.append(i[0])
-    while len(answer) != N:
-        if len(answer) not in answer:
-            answer.append(len(answer))
-        else:
-            answer.append(len(answer)+1)
-    return answer
+            bin[i] = 0
+        user-=stages.count(i)
+    return sorted(bin, key=lambda x: bin[x], reverse=True)
