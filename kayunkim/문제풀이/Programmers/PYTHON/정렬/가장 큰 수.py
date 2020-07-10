8fffc0ae1884910c7ff5be9d511f4ef00816fa7f
KaYunKIM <gayun1109@hanmail.net>
2020-07-11 01:28:27 +0900

    2020.07.11

--- kayunkim/문제풀이/Programmers/PYTHON/정렬/가장 큰 수.py
+++ kayunkim/문제풀이/Programmers/PYTHON/정렬/가장 큰 수.py
@@ -1,27 +1,4 @@
-# def solution(numbers):
-#     answer= 0
-#     i = 0
-#     while numbers:
-#         max,first = 0,0
-#         for n in numbers:
-#             if first <= int(str(n)[0]) or max < int(str(n)):
-#                 max, first = n, int(str(n)[0])
-#         print('max',max)
-#         # print(numbers.pop())
-#         numbers.remove(max)
-#     return answer
-#
-#
-#
-#
-# def solution(numbers):
-#     for n in numbers:
-#         temp = sorted(numbers,key = lambda n:str(n)[0], reverse=True)
-#     return (''.join(map(str,temp)))
-
-
 def solution(numbers):
     numbers = list(map(str,numbers))
-    # temp = sorted(num, key = lambda x:x[0], reverse=True)
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    return ''.join(numbers)
+    numbers = sorted(numbers, key=lambda x:x*3, reverse=True)
+    return str(int(''.join(numbers)))
