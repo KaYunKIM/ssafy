96f5451bb9fd313d10c5e3b8924a15405901690d
KaYunKIM <gayun1109@hanmail.net>
2020-07-11 22:08:43 +0900

    2020.07.11

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/피보나치 수.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/피보나치 수.py
@@ -4,6 +4,16 @@ def solution(n):
         if i<=1:
             bin.append(i)
         else:
-            bin.append(bin[-1]%1234567+bin[-2]%1234567)
+            bin.append(bin[-1]+bin[-2])
     answer = bin[-1]%1234567
     return answer
+
+
+def solution(n):
+    answer =[0]*(n+1)
+    for i in range(n+1):
+        if i <=1:
+            answer[i]=i
+        else:
+            answer[i]= answer[i-1]+answer[i-2]
+    return answer[n]%1234567
