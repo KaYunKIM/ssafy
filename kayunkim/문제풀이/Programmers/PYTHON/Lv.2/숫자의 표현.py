aca0ed47d68cd2d89d346bf270ce8ea9715fa046
KaYunKIM <gayun1109@hanmail.net>
2020-07-15 00:12:27 +0900

    2020.07.15

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/숫자의 표현.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/숫자의 표현.py
@@ -13,3 +13,28 @@ def solution(n):
                 break
         cur+=1
     return cnt
+
+
+# 테스트 1 〉	통과 (7.55ms, 10.7MB)
+# 테스트 2 〉	통과 (4.41ms, 10.7MB)
+# 테스트 3 〉	통과 (5.07ms, 10.7MB)
+# 테스트 4 〉	통과 (4.98ms, 10.6MB)
+# 테스트 5 〉	통과 (5.77ms, 10.6MB)
+# 테스트 6 〉	통과 (5.68ms, 10.7MB)
+
+
+def solution(n):
+    cnt=0
+    cur=1
+    while cur != n:
+        temp=0
+        for i in range(cur,n//2+2):
+            if temp+i<=n:
+                temp+=i
+                if temp==n:
+                    cnt+=1
+                    break
+            else:
+                break
+        cur+=1
+    return cnt+1
