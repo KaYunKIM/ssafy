c104bc455b39fc96c6499d44bc6f3770cd7a737c
KaYunKIM <gayun1109@hanmail.net>
2020-07-18 01:40:42 +0900

    2020.07.18

--- kayunkim/문제풀이/Programmers/PYTHON/스택,큐/다리를 지나는 트럭.py
+++ kayunkim/문제풀이/Programmers/PYTHON/스택,큐/다리를 지나는 트럭.py
@@ -1,43 +1,119 @@
-# import copy
 # def solution(bridge_length, weight, truck_weights):
 #     answer = 0
 #     on = []
-#     finished = []
-#     B = copy.deepcopy(bridge_length)
-#     print(bridge_length)
-#     while len(truck_weights)!=0 and len(on) !=0:
+#     finish = []
+#     bin = [0]*len(truck_weights)
+#     t=0
+#     while t != len(truck_weights)-1:
+#         if bin[t] == bridge_length:
+#             finish.append(on.pop(0))
+#             t+=1
+#         print('t',t)
+#         if sum(on)+truck_weights[t]<=weight:
+#             on.append(truck_weights[t])
+#         for i in range(t+1):
+#             bin[i]+=1
+#         print(on, bin)
+# #         if cnt==bridge_length:
+# #             finish.append(on.pop(0))
+# #             cnt -= 1
+# #         if sum(on)+t<=weight:
+# #             on.append(t)
 #
-#         # if sum(on) + truck_weights[0] <= weight:
-#         #     on.append((truck_weights.pop(0))
-#         print(bridge_length)
-#         # bridge_length-=1
-#         # if bridge_length == 0:
-#         #     finished.append(on.pop(0))
-#         # bridge_length = B
-#         # answer += 1
+# #         else:
+# #             answer+=1
+# #             if sum(on)+t<=weight:
+# #                 on.append(t)
+# #         cnt+=1
+# #         answer+=1
 #     return answer
+
+# def solution(bridge_length, weight, truck_weights):
+#     answer = 1
+#     on = []
+#     finish = []
+#     for t in truck_weights:
+#         cnt=0
+#         while finish != truck_weights:
+#             if cnt==bridge_length:
+#                 finish.append(on.pop(0))
+#                 cnt = 0
+#             if sum(on)+t<=weight:
+#                 on.append(t)
+#             else:
+#                 cnt+=1
+#                 on.append(t)
+#             cnt+=1
+#             print(answer)
+#     return answer
+
+
+import copy
+
+# def solution(bridge_length, weight, truck_weights):
+#     answer = 0
+#     on = []
+#     fin = []
+#     rep = copy.deepcopy(bridge_length)
+#     v = [0]*len(truck_weights)
+#     h = 0
+#     while fin != truck_weights:
+#         bridge_length = rep
 #
-# bridge_length = 2
-# weight = 10
-# truck_weights = [7,4,5,6]
-# solution(bridge_length, weight, truck_weights)
+#         # while bridge_length != 0:
+#             print(answer,fin,on, bridge_length)
+#             if sum(on) + truck_weights[h] <= weight:
+#                 on.append(truck_weights[h])
+#                 for i in range(h+1):
+#                     v[i]+=1
+#             bridge_length -= 1
+#             answer+=1
+#
+#         print(answer,fin,on,v)
+#         fin.append(on.pop(0))
+#         h += 1
+
+def solution(bridge_length, weight, truck_weights):
+    on = []
+    fin = []
+    v = [0]*len(truck_weights)
+    h=0
+    while fin != truck_weights:
+        if h<len(truck_weights):
+            if sum(on)+truck_weights[h] <= weight:
+                on.append(truck_weights[h])
+                h+=1
+        for i in range(h):
+            v[i]+=1
+        print(h, fin, on, v)
+        if bridge_length in v:
+            fin.append(on.pop(0))
+        print('ans', h, fin, on, v)
+        if h==len(truck_weights):
+            v[0] += bridge_length
+            return v[0]
+        # else:
+        #
+
+    print(fin,v[0]+1)
+
+
+print(solution(2,10,[7,4,5,6]))
+# solution(100,100,[10])
 
 
 def solution(bridge_length, weight, truck_weights):
-    answer = 1
     on = []
-    finish = []
-    for t in truck_weights:
-        cnt=0
-        while finish != truck_weights:
-            if cnt==bridge_length:
-                finish.append(on.pop(0))
-                cnt = 0
-            if sum(on)+t<=weight:
-                on.append(t)
-            else:
-                cnt+=1
-                on.append(t)
-            cnt+=1
-            print(answer)
-    return answer
+    fin = []
+    v = [0]*len(truck_weights)
+    h=0
+    while fin != truck_weights:
+        if h<len(truck_weights):
+            if sum(on)+truck_weights[h] <= weight:
+                on.append(truck_weights[h])
+                h+=1
+        for i in range(h):
+            v[i]+=1
+        if bridge_length in v:
+            fin.append(on.pop(0))
+    return v[0]+1
