7959e9264bb56b393510c83d67331ff92267c770
KaYunKIM <gayun1109@hanmail.net>
2020-07-22 01:02:47 +0900

    2020.07.22

--- kayunkim/문제풀이/Programmers/PYTHON/탐욕법/큰 수 만들기.py
+++ kayunkim/문제풀이/Programmers/PYTHON/탐욕법/큰 수 만들기.py
@@ -1,15 +1,12 @@
-import copy
 def solution(number, k):
-    number = list(number)
     cur = 1
-    ans_len = copy.deepcopy(len(number))
+    ans_len = len(number)
     while len(number)!= ans_len-k and cur!= len(number):
         if int(number[cur-1]) < int(number[cur]):
             h = cur
             while h!=0 and int(number[h-1]) < int(number[h]) and len(number)> ans_len-k:
                 # print(h,int(number[h-1]),int(number[h]))
-                number.del(number[h-1])
-                # number = number[0:h-1] + number[h:]
+                number = number[0:h-1] + number[h:]
                 cur-=1
                 h-=1
         cur+=1
@@ -17,6 +14,3 @@ def solution(number, k):
     if len(number) == ans_len:
         return ''.join(number[:len(number)-k])
     return ''.join(number)
-
-print(solution("4177252841", 4))
-print(solution("999999", 2))
