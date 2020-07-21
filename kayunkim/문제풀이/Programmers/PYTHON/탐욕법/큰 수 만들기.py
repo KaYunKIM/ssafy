13180790f3e464198b07ac73ad2d389f0e3b91f7
KaYunKIM <gayun1109@hanmail.net>
2020-07-22 00:59:06 +0900

    2020.07.22

--- kayunkim/문제풀이/Programmers/PYTHON/탐욕법/큰 수 만들기.py
+++ kayunkim/문제풀이/Programmers/PYTHON/탐욕법/큰 수 만들기.py
@@ -2,17 +2,21 @@ import copy
 def solution(number, k):
     number = list(number)
     cur = 1
-    ans_len = copy.deepcopy(len(number))-k
-    while len(number)!= ans_len:
+    ans_len = copy.deepcopy(len(number))
+    while len(number)!= ans_len-k and cur!= len(number):
         if int(number[cur-1]) < int(number[cur]):
             h = cur
-            while h!=0 and int(number[h-1]) < int(number[h]) and len(number)> ans_len:
-                print(h,int(number[h-1]),int(number[h]))
-                number.remove(number[h-1])
+            while h!=0 and int(number[h-1]) < int(number[h]) and len(number)> ans_len-k:
+                # print(h,int(number[h-1]),int(number[h]))
+                number.del(number[h-1])
+                # number = number[0:h-1] + number[h:]
                 cur-=1
                 h-=1
         cur+=1
-        print(number)
+        # print(number)
+    if len(number) == ans_len:
+        return ''.join(number[:len(number)-k])
     return ''.join(number)
 
 print(solution("4177252841", 4))
+print(solution("999999", 2))
