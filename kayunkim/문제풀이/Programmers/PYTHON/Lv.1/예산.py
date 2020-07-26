59c522eb09bd8edfafce6bbcfb339e7ad64d7e58
KaYunKIM <gayun1109@hanmail.net>
2020-07-26 23:02:20 +0900

    2020.07.26

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.1/예산.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.1/예산.py
@@ -1,21 +1,12 @@
-cnt = 0
-def solution(d, budget):
-    if sum(d)<= budget:
-        return len(d)
-    else:
-        visited = [0]*len(d)
-        def find(cur,sumV,budget):
-            global cnt
-            # print(cur,sumV)
-            cnt = cur
-            if sumV > budget:
-                return
-            else:
-                for i in range(len(d)):
-                    if not visited[i]:
-                        visited[i]=1
-                        find(cur+1,sumV+d[i],budget)
-                        visited[i]=0
+import heapq
 
-        find(0,0,budget)
-        return cnt
+def solution(d, budget):
+    cnt = 0
+    heapq.heapify(d)
+    while d:
+        if  budget - d[0] >=0:
+            budget-= heapq.heappop(d)
+            cnt+=1
+        else:
+            return cnt
+    return cnt
