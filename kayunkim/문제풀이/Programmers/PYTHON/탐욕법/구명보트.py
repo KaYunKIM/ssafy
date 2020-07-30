252d3d7ebec8e464af30b3c35847bc13840e7abe
KaYunKIM <gayun1109@hanmail.net>
2020-07-30 23:18:03 +0900

    2020.07.30

--- kayunkim/문제풀이/Programmers/PYTHON/탐욕법/구명보트.py
+++ kayunkim/문제풀이/Programmers/PYTHON/탐욕법/구명보트.py
@@ -1,44 +1,40 @@
-# import heapq
+# # import heapq
 # def solution(people, limit):
 #     answer = 0
-#     h = 0
-#     while people:
+#     # for i in range(0,len(people)-1):
+#     while len(people):
 #         maxV = 0
-#         temp = heapq.heappop(people)
-#         if temp!=0:
-#             if limit-temp < 40:
-#                 answer+=1
-#             else:
-#                 if len(people)==1:
-#                     answer+=1
-#                 i=1
-#                 cur = 0
-#                 # print(temp, people[i])
-#                 while heapq.heapify(people):
-#                     if maxV < people[i] <= limit-temp:
-#                         maxV, cur = people[i],i
-#                     i+=1
-#                 # print(maxV, cur)
-#                 people[cur]=0
-#                 # print(people)
-#                 answer+=1
-#         h+=1
+#         if limit-people[0]<40:
+#             answer+=1
+#         elif people[0]!=0:
+#             for j in range(1,len(people)):
+#                 if maxV< people[j] <= limit-people[0]:
+#                     maxV, cur = people[j], j
+#             if maxV != 0:
+#                 people.remove(people[cur])
+#             answer+=1
+#         # people = people
+#     if people[-1]!=0:
+#         answer+=1
 #     return answer
+
+
+import heapq
 def solution(people, limit):
-    answer = 0
-    # for i in range(0,len(people)-1):
-    while len(people):
-        maxV = 0
-        if limit-people[0]<40:
-            answer+=1
-        elif people[0]!=0:
-            for j in range(1,len(people)):
-                if maxV< people[j] <= limit-people[0]:
-                    maxV, cur = people[j], j
-            if maxV != 0:
-                people.remove(people[cur])
-            answer+=1
-        # people = people
-    if people[-1]!=0:
-        answer+=1
-    return answer
+    sumV = 0
+    cur = 0
+    cnt = 0
+    while people:
+        heapq.heapify(people)
+        if sumV + people[0] <= limit:
+            sumV+= people.pop(0)
+            cur+=1
+            # print('1',people, sumV, cnt)
+        else:
+            sumV = 0
+            cnt+=1
+        if cur == 2:
+            sumV = 0
+            cnt+=1
+            # print('2',people, sumV, cnt)
+    return cnt+1
