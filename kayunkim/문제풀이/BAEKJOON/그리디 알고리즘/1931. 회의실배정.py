5c655e0e49b19f5e6c00d746bad4e437849295b5
KaYunKIM <gayun1109@hanmail.net>
2020-09-10 00:18:18 +0900

    2020.09.10

--- kayunkim/문제풀이/BAEKJOON/그리디 알고리즘/1931. 회의실배정.py
+++ kayunkim/문제풀이/BAEKJOON/그리디 알고리즘/1931. 회의실배정.py
@@ -4,12 +4,10 @@ timetable.sort()
 ans = 1
 minV = 2**31
 for i in range(len(timetable)):
-    if timetable[i][1] <= minV:
+    if timetable[i][1] < minV:
         minV = timetable[i][1]
     else:
         if timetable[i][0]>= minV:
             ans+=1
-            minV = 2**31
-            if timetable[i][1] <= minV:
-                minV = timetable[i][1]
+            minV = timetable[i][1]
 print(ans)
