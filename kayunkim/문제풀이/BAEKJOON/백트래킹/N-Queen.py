1082ec5d3314aa4a6cd15bd43fcbc4452ca6ea56
KaYunKIM <gayun1109@hanmail.net>
2020-10-19 22:51:25 +0900

    2020.10.19

--- kayunkim/문제풀이/BAEKJOON/백트래킹/N-Queen.py
+++ kayunkim/문제풀이/BAEKJOON/백트래킹/N-Queen.py
@@ -1,37 +1,31 @@
-def find(cur_i, cur_j, sumv):
+def find(cur):
     global cnt, N
-    print(cur_i, cur_j, sumv, sumV)
-    if sumv == N-1:
+    print(cur,sumV)
+    if cur == N:
         cnt+=1
         print()
         return
     else:
-        for i in range(cur_i+1, N):  # 깊이탐색
-            for j in range(N):
-                d1_idx = N-1-(i-j)
-                d2_idx = i+j
-                # print('mid',i,j, d1_idx, d2_idx)
-                if not v[j] and not diag1[d1_idx] and not diag2[d2_idx]:
-                    v[j] = 1
-                    diag1[d1_idx]=1
-                    diag2[d2_idx]=1
-                    sumV.append((i,j))
-                    find(i,j, sumv+1)
-                    v[j] = 0
-                    diag1[d1_idx] = 0
-                    diag2[d2_idx] = 0
-                    sumV.pop()
+        for i in range(N):  #가로탐색
+            d1_idx = N-1-(cur-i)
+            d2_idx = cur+i
+            print('mid',cur,i, d1_idx, d2_idx)
+            if not v[i] and not diag1[d1_idx] and not diag2[d2_idx]:
+                v[i] = 1
+                diag1[d1_idx]=1
+                diag2[d2_idx]=1
+                sumV.append((cur,i))
+                find(cur+1)
+                v[i] = 0
+                diag1[d1_idx] = 0
+                diag2[d2_idx] = 0
+                sumV.pop()
 
 N = int(input())
 cnt = 0
-v = [0]*N
+v = [0]*N  #가로
 sumV = []
-diag1 = [0]*(2*N-1)
-diag2 = [0]*(2*N-1)
-for i in range(N):
-    v[i] = 1
-    diag1[N-1+i] = 1
-    diag2[i] = 1
-    sumV.append((0,i))
-    find(0, i, 0)
+diag1 = [0]*(2*N-1)  #왼쪽대각선
+diag2 = [0]*(2*N-1)  #오른쪽대각선
+find(0)
 print(cnt)
