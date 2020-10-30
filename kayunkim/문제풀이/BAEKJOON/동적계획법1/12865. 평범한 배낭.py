72c7383dcc51a6a3f2b2429c96fb29bf3613b3d1
KaYunKIM <gayun1109@hanmail.net>
2020-10-30 23:42:58 +0900

    2020.10.30

---
+++ kayunkim/문제풀이/BAEKJOON/동적계획법1/12865. 평범한 배낭.py
@@ -0,0 +1,19 @@
+N,K = map(int,input().split())
+bag = [list(map(int,input().split())) for _ in range(N)]
+maxV = [0]*N
+weight = [0]*N
+for i in range(N):
+    if i == 0:
+        weight[i] = bag[i][0]
+        maxV[i] = bag[i][1]
+    else:
+        if maxV[-1]+bag[i][0] <= K:
+            w = maxV[-1]+bag[i][0]
+            m = maxV[-1] + bag[i][1]
+        else:
+            if i == 1:
+                w = max(weight[-1], )
+                maxV[i] = maxV[-1]
+            else:
+                w = max(weight[-1], weight[]
+                maxV[i] = maxV[-1]
