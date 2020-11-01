607eca607d27bc24af7111779a1af7072606748f
KaYunKIM <gayun1109@hanmail.net>
2020-11-01 23:50:35 +0900

    2020.11.01

--- kayunkim/문제풀이/BAEKJOON/브루트포스/2231. 분해합.py
+++ kayunkim/문제풀이/BAEKJOON/브루트포스/2231. 분해합.py
@@ -1,17 +1,33 @@
-N = input()
-minV = 100000000
-if len(N)==1:
-    print('0')
-else:
-    for i in range(int(N)-1,int(N)//2,-1):
-        sumV = i
-        for j in range(len(str(i))):
-            sumV+=int(str(i)[j])
-            if sumV >int(N):
-                break
-        if sumV == int(N) and i < minV:
-            minV = i
-    if minV == 100000000:
-        print('0')
-    else:
-        print(minV)
+N = int(input())
+minV = 1000000000
+check = N
+while check>N//2:
+    sumup = 0
+    num = check
+    while num>0:
+        sumup+=num%10
+        num //=10
+    if check+sumup ==N and check< minV:
+        minV = check
+    check-=1
+print(minV if minV !=1000000000 else '0')
+
+
+
+# N = input()
+# minV = 100000000
+# if len(N)==1:
+#     print('0')
+# else:
+#     for i in range(int(N)-1,int(N)//2,-1):
+#         sumV = i
+#         for j in range(len(str(i))):
+#             sumV+=int(str(i)[j])
+#             if sumV >int(N):
+#                 break
+#         if sumV == int(N) and i < minV:
+#             minV = i
+#     if minV == 100000000:
+#         print('0')
+#     else:
+#         print(minV)
