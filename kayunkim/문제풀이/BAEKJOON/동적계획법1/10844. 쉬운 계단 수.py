03afd19804d483937dc144c3db96af2c9a286d9b
KaYunKIM <gayun1109@hanmail.net>
2020-11-15 01:42:57 +0900

    2020.11.15

--- kayunkim/문제풀이/BAEKJOON/동적계획법1/10844. 쉬운 계단 수.py
+++ kayunkim/문제풀이/BAEKJOON/동적계획법1/10844. 쉬운 계단 수.py
@@ -1,14 +1,15 @@
 N = int(input())
-nc = [[0]*10
-for _ in range(inputNum+1)]
-ans, mod = 0, 1000000000
-for i in range(1, 10):
-    nc[1][i] = 1
-for i in range(2, inputNum+1):
-    for j in range(0, 10):
-        if j > 0:
-            nc[i][j] += nc[i-1][j-1]
-        if j < 9:
-            nc[i][j] += nc[i-1][j+1]
-        nc[i][j] %= mod
-print(sum(nc[inputNum])%mod)
+cnt = 0
+num = [[0]*10 for _ in range(N)]
+for i in range(1,10):
+    num[0][i] = 1
+for i in range(1,N):
+    for j in range(10):
+        if j == 0:
+            num[i][j] += num[i - 1][j + 1]
+        elif j == 9:
+            num[i][j] += num[i - 1][j - 1]
+        else:
+            num[i][j] += num[i-1][j-1]
+            num[i][j] += num[i-1][j+1]
+print(sum(num[-1])%1000000000)
