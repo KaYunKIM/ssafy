dafda110baade192dd60f2b6676a7a8d0df62e3d
KaYunKIM <gayun1109@hanmail.net>
2020-11-21 23:54:59 +0900

    2020.11.21

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/124 나라의 숫자.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/124 나라의 숫자.py
@@ -1,13 +1,21 @@
+# def solution(n):
+#     answer = []
+#     temp = [4,1,2]
+#     while n>2:
+#         answer.insert(0,temp[n%3])
+#         print('1',answer)
+#         n//=3
+#         print('1',n)
+#     if n!=0:
+#         print(n)
+#         answer.insert(0,temp[n])
+#         print('2',answer)
+#     return answer
+
 def solution(n):
-    answer = []
-    temp = [4,1,2]
-    while n>2:
-        answer.insert(0,temp[n%3])
-        print('1',answer)
-        n//=3
-        print('1',n)
-    if n!=0:
-        print(n)
-        answer.insert(0,temp[n])
-        print('2',answer)
+    answer = ''
+    while n > 0:
+        n -= 1
+        answer = '124'[n%3] + answer
+        n //= 3
     return answer
