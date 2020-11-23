c3f0d3d97ddc2f1c74acdbd71956adf1ab59370e
KaYunKIM <gayun1109@hanmail.net>
2020-11-24 01:05:08 +0900

    2020.11.24

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/영어 끝말잇기.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/영어 끝말잇기.py
@@ -1,9 +1,14 @@
 def solution(n, words):
-    answer = [0, 0]
-    for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0] or words[i] in words[0:i]:
-            answer[0] = ( i % n ) + 1
-            answer[1] = ( i // n ) + 1
-            break
-    return answer
-
+    answer = [0,0]
+    temp = {}
+    for i in range(len(words)):
+        answer[0] = i%n+1
+        answer[1] = i//n+1
+        if words[i] not in temp:
+            temp[words[i]] = 1
+        else:
+            return answer
+        if i != 0:
+            if words[i][0] != words[i-1][-1]:
+                return answer
+    return [0,0]
