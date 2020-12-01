66a2124e8d376a6046faf325676fc74a4114003c
KaYunKIM <gayun1109@hanmail.net>
2020-12-01 23:44:06 +0900

    2020.12.01

--- kayunkim/문제풀이/BAEKJOON/동적계획법1/2565. 전깃줄.py
+++ kayunkim/문제풀이/BAEKJOON/동적계획법1/2565. 전깃줄.py
@@ -1,9 +1,20 @@
 N = int(input())
-poleA = []
-poleB = []
+pole = []
+poleMaxV = 0
 for _ in range(N):
     A,B = map(int,input().split())
-    poleA.append(A)
-    poleB.append(B)
-poleA.sort()
-print(poleA, poleB)
+    if B > poleMaxV:
+        poleMaxV = B
+    pole.append([A,B])
+pole.sort()
+LIS = [0]*(poleMaxV+1)
+ans = 0
+for i in pole:
+    maxV = 0
+    for j in range(1,i[1]+1):
+        if LIS[j] > maxV:
+            maxV = LIS[j]
+    LIS[i[1]] = maxV+1
+    if LIS[i[1]] > ans:
+        ans = LIS[i[1]]
+print(N-ans)
