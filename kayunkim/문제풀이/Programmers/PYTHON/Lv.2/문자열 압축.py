0c36527928d5970f3a6730fd2953d3acc341f082
KaYunKIM <gayun1109@hanmail.net>
2020-12-06 23:53:23 +0900

    2020.12.06

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/문자열 압축.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/문자열 압축.py
@@ -14,11 +14,38 @@ def solution(s):
                 else:
                     temp.append(h)
                     temp.append(s[i:i+h])
-        print(temp)
         while 1 in temp:
             temp.remove(1)
         if len(temp)< minV:
             minV = len(temp)
-            print(minV)
         h+=1
     return minV
+
+
+def solution(s):
+    minV = 10000000000
+    h = 1
+    while h != len(s):
+        temp = []
+        for i in range(0,len(s),h):
+            if not temp:
+                temp.append(1)
+                temp.append(s[i:i+h])
+            else:
+                if temp[-1] == s[i:i+h]:
+                    temp[-2]+=1
+                else:
+                    temp.append(1)
+                    temp.append(s[i:i+h])
+            # print(temp)
+        while 1 in temp:
+            temp.remove(1)
+        newtemp = ''.join(map(str,temp))
+        # print(newtemp, len(newtemp), temp, len(temp))
+        if len(newtemp) < minV:
+            minV = len(newtemp)
+        h+=1
+    if minV == 10000000000:
+        return 1
+    else:
+        return minV
