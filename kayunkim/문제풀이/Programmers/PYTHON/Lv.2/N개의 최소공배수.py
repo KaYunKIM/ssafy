514ec2a129a82203dec87f6e7025f3f785ef2e64
KaYunKIM <gayun1109@hanmail.net>
2020-12-27 23:41:56 +0900

    2020.12.27

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.2/N개의 최소공배수.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/N개의 최소공배수.py
@@ -3,9 +3,9 @@ def solution(arr):
     for i in range(len(arr)-1):
         num1, num2 = arr[i], arr[i+1]
         while num1%num2:
-            remain = num1%num2 #6
+            remain = num1%num2
             num1, num2 = num2, remain
-            print(num1,num2)
-        print(num1%num2, num1//num2, (arr[i]*arr[i+1])/num2)
+            # print(num1,num2)
+        # print(num1%num2, num1//num2, (arr[i]*arr[i+1])/num2)
         arr[i+1] = (arr[i]*arr[i+1])/num2
     return arr[-1]
