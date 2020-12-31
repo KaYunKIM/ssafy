1534cf42406d869e43a0fa783dbebb190d0f4d9d
KaYunKIM <gayun1109@hanmail.net>
2020-12-31 22:11:18 +0900

    2020.12.31

---
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.2/포켓몬.py
@@ -0,0 +1,9 @@
+def solution(nums):
+    answer = 0
+    length = len(nums) // 2
+    temp = list(set(nums))
+
+    for value in temp:
+        if (answer < length):
+            answer += 1
+    return answer
