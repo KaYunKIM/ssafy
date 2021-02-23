af16e3f6ccb19a48c5dbb09cbcb56fb73db61dbd
KaYunKIM <gayun1109@hanmail.net>
2021-02-23 10:55:53 +0900

    Modify 소수 찾기.py

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.1/소수 찾기.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.1/소수 찾기.py
@@ -1,11 +1,18 @@
+## 시간초과
 def solution(n):
     answer = 1
     for i in range(3,n+1):
-        cnt = 0
         for j in range(2,i):
             if i%j ==0:
-                cnt-=1
                 break
-        if cnt==0:
+        else:
             answer+=1
     return answer
+
+
+def solution(n):
+    nums = set([i for i in range(3,n+1,2)])
+    for i in range(3,n+1,2):
+        if i in nums:
+            nums-=set([j for j in range(i*2,n+1,i)])
+    return len(nums)+1
