20b17edda9ddcbfba5b30e77863e1d40c4571824
KaYunKIM <gayun1109@hanmail.net>
2021-03-08 23:20:04 +0900

    Modify 순위.py

--- kayunkim/문제풀이/Programmers/PYTHON/Lv.3/순위.py
+++ kayunkim/문제풀이/Programmers/PYTHON/Lv.3/순위.py
@@ -19,11 +19,32 @@ def solution(n, results):
                 if cur not in graph or len(graph[cur]) == 0:
                     stack.pop()
                     v[cur] = 1
-                    
+
                 else:
                     if not v[graph[cur][0]]:
                         stack.append(graph[cur].pop(0))
                     else:
                         stack.pop()
 
+    return answer
+
+
+def solution(n, results):
+    answer = 0
+
+    v = [0] * (n + 1)
+    cur = [0] * (n + 1)
+
+    for i in results:
+        cur[i[0]] = i[1]
+        v[i[1]] += 1
+
+    for i in range(1, n + 1):
+        if not cur[i]:
+            answer += 1
+        else:
+            if v[cur[i]] == 1:
+                answer += 1
+
+    # print(v, cur)
     return answer
