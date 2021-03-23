e233eec6101ff864346eeff18ca02d8cae686e89
KaYunKIM <gayun1109@hanmail.net>
2021-03-23 22:56:40 +0900

    Add N으로 표현.py

---
+++ kayunkim/문제풀이/Programmers/PYTHON/동적계획법/N으로 표현.py
@@ -0,0 +1,22 @@
+def solution(N, number):
+    answer = 10000000
+
+    def find(cur, sumV):
+        nonlocal answer, N, number
+        if sumV > answer:
+            return
+
+        if cur == number:
+            if sumV < answer:
+                answer = sumV
+            return
+
+        find(cur + N, sumV + 1)
+        find(cur - N, sumV + 1)
+        find(cur * N, sumV + 1)
+        find(cur // N, sumV + 1)
+
+    for i in range(1, len(str(number)) + 1):
+        num = int(str(N) * i)
+        find(num, 1)
+    return answer
