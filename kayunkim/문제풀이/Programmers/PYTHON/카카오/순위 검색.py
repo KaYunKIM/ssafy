24d4fb1b216d0098c6686bf71a0d7de852666025
KaYunKIM <gayun1109@hanmail.net>
2021-04-03 23:17:26 +0900

    Add 순위 검색.py

---
+++ kayunkim/문제풀이/Programmers/PYTHON/카카오/순위 검색.py
@@ -0,0 +1,15 @@
+def solution(info, query):
+    answer = [0] * len(query)
+    for q in range(len(query)):
+        reform = query[q].replace('and ', '').replace('- ', '').split(' ')
+        for i in info:
+            i = i.split(' ')
+            if len(reform) > 1:
+                if set(reform[:-1] + i[:-1]) == set(i[:-1]):
+                    if int(i[-1]) >= int(reform[-1]):
+                        answer[q] += 1
+            else:
+                if int(i[-1]) >= int(reform[0]):
+                    answer[q] += 1
+
+    return answer
